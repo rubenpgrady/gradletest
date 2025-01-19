0380cfcc806de0f799c2b7d82b78d8f437ac06ee
rubenpgrady <125112763+rubenpgrady@users.noreply.github.com>
2025-01-19 16:51:25 +0100

    Change text from MainActivity.java

--- app/src/main/java/org/gradle/samples/MainActivity.java
+++ app/src/main/java/org/gradle/samples/MainActivity.java
@@ -1,8 +1,8 @@
 package org.gradle.samples;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView1 = findViewById(R.id.textView1);
+        textView1.setText("This is the new text!");
     }
+
 }
