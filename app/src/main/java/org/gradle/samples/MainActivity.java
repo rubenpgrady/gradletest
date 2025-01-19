1f7be5df87ec7a93175debbf9e3e631309fe0699
rubenpgrady <125112763+rubenpgrady@users.noreply.github.com>
2025-01-20 00:13:12 +0100

    PackageManager code (#22)
    * PackageManager code
    
    * Update MainActivity.java

--- app/src/main/java/org/gradle/samples/MainActivity.java
+++ app/src/main/java/org/gradle/samples/MainActivity.java
@@ -3,6 +3,9 @@ package org.gradle.samples;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageInfo;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,8 +13,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        StringBuilder output = new StringBuilder();
+        PackageManager pm = getPackageManager();
+        // Refer to https://developer.android.com/reference/android/content/pm/PackageManager.html#getInstalledPackages(int)
+        // Quote: If flag MATCH_UNINSTALLED_PACKAGES is set, the package
+        // information is retrieved from the list of uninstalled applications
+        // (which includes installed applications as well as applications
+        // with data directory i.e. applications which had been deleted
+        // with DELETE_KEEP_DATA flag set).
+        /*List<ApplicationInfo> applications = pm.getInstalledPackages(PackageManager.MATCH_UNINSTALLED_PACKAGES);
+        for (ApplicationInfo info : applications) {
+            // Refer to https://developer.android.com/reference/android/content/pm/ApplicationInfo
+            output.append(appName);
+            // TODO nativeLibraryDir
+            output.append("\n");
+        }
+        output.append("\n");*/
+        List<PackageInfo> packages = pm.getInstalledPackages(PackageManager.MATCH_UNINSTALLED_PACKAGES);
+        for (PackageInfo info : packages) {
+            // Refer to https://developer.android.com/reference/android/content/pm/PackageInfo
+            output.append(info.packageName);
+            if (info.applicationInfo != null) {
+                output.append(" -> ");
+                output.append(info.applicationInfo.nativeLibraryDir);
+            }
+            output.append("\n");
+        }
         TextView textView1 = findViewById(R.id.textView1);
-        textView1.setText("This is the new text!");
+        textView1.setText(output.toString());
     }
 
 }
