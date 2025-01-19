7a73e4d43bb7cabfe32219b1b485c9dfc5bffd94
rubenpgrady <125112763+rubenpgrady@users.noreply.github.com>
2025-01-19 21:31:11 +0100

    PackageManager code

--- app/src/main/java/org/gradle/samples/MainActivity.java
+++ app/src/main/java/org/gradle/samples/MainActivity.java
@@ -3,6 +3,7 @@ package org.gradle.samples;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.content.pm.PackageManager;
 
 public class MainActivity extends AppCompatActivity {
 
